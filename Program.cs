6c4445abc5dfdb94ca6c409698d58398b1adb3b3
sandip ahire <sandipahire34@gmail.com>
2021-09-06 21:13:31 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Pattern1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j, n = 5;
+            for (i = 0; i <= n; i++)
+            {
+                for (j = 1; j <= i; j++)
+                {
+                    Console.Write(" " + j);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+/*OUTPUT:
+ 1
+ 1 2
+ 1 2 3
+ 1 2 3 4
+ 1 2 3 4 5
+*/
